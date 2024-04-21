521aa71c33a7ce40a09d79a7d353731e5688a7f1
Ruben Torres <rubenbebe.145.rt@gmail.com>
2024-04-21 17:40:14 -0600

    Actualización de la funcionalidad de resetear contraseña de empleado en auth.empleado.controller.js y auth.empleado.routes.js

--- src/controller/auth.empleado.controller.js
+++ src/controller/auth.empleado.controller.js
@@ -1,61 +1,6 @@
 import { Empleado } from "../model/all.models.js";
 import jwt from 'jsonwebtoken';
 
-export const crear = async (req, res) => {
-    try {
-
-        const { noEmpleado, usuario, password } = req.body;
-
-        const empleadoExiste = await Empleado.findOne({ noEmpleado });
-        if (empleadoExiste) {
-            return res.status(400).json({ message: 'El empleado ya existe' });
-        }
-
-        const nuevoEmpleado = new Empleado({ noEmpleado, usuario, password });
-        await nuevoEmpleado.save();
-        res.status(201).json({ message: 'Empleado creado' });
-
-    } catch (error) {
-        res.status(500).json({ message: 'Error al crear el empleado' });
-    }
-}
-
-export const obtenerEmpleados = async (req, res) => {
-    try {
-        const empleados = await Empleado.find();
-        res.status(200).json(empleados);
-    } catch (error) {
-        res.status(500).json({ message: 'Error al obtener los empleados' });
-    }
-}
-
-export const obtenerEmpleado = async (req, res) => {
-    try {
-        const empleado = await Empleado.findById(req.params.id);
-        res.status(200).json(empleado);
-    } catch (error) {
-        res.status(500).json({ message: 'Error al obtener el empleado' });
-    }
-}
-
-export const actualizarEmpleado = async (req, res) => {
-    try {
-        await Empleado.findByIdAndUpdate(req.params.id, req.body);
-        res.status(200).json({ message: 'Empleado actualizado' });
-    } catch (error) {
-        res.status(500).json({ message: 'Error al actualizar el empleado' });
-    }
-}
-
-export const eliminarEmpleado = async (req, res) => {
-    try {
-        await Empleado.findByIdAndDelete(req.params.id);
-        res.status(200).json({ message: 'Empleado eliminado' });
-    } catch (error) {
-        res.status(500).json({ message: 'Error al eliminar el empleado' });
-    }
-}
-
 export const login = async (req, res) => {
     try {
         const { usuario, password } = req.body;
@@ -73,5 +18,40 @@ export const login = async (req, res) => {
         }
     } catch (error) {
         res.status(500).json({ message: 'Error al iniciar sesión' });
+    }
+}
+
+//resetear contraseña de empleado
+export const actualizarPassword = async (req, res) => {
+    try {
+        const { usuario, password } = req.body;
+
+        const existing = await Empleado.findOne({ usuario });
+        if (existing) {
+            // Verificar si la contraseña nueva es igual a la anterior
+            if (existing.password === password) {
+                return res.status(400).json({ message: 'La contraseña es igual a la anterior' });
+            }
+
+            existing.password = password;
+            await existing.save();
+            return res.status(200).json({ message: 'Contraseña actualizada' });
+        }
+
+        return res.status(400).json({ message: 'El usuario no existe' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error al actualizar contraseña' });
+    }
+}
+
+//obtener los empleados
+export const obtenerEmpleados = async (req, res) => {
+    try {
+        const empleados = await Empleado.find();
+        return res.status(200).json(empleados);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error al obtener empleados' });
     }
 }
--- src/routes/auth.empleado.routes.js
+++ src/routes/auth.empleado.routes.js
@@ -3,11 +3,7 @@ import * as authEmpleadoController from "../controller/auth.empleado.controller.
 
 const router = Router();
 
-router.post('/crear', authEmpleadoController.crear);
 router.post('/login', authEmpleadoController.login);
+router.put('/actualizarPassword', authEmpleadoController.actualizarPassword);
 router.get('/obtenerEmpleados', authEmpleadoController.obtenerEmpleados);
-router.get('/obtenerEmpleado/:id', authEmpleadoController.obtenerEmpleado);
-router.put('/actualizarEmpleado/:id', authEmpleadoController.actualizarEmpleado);
-router.delete('/eliminarEmpleado/:id', authEmpleadoController.eliminarEmpleado);
-
 export default router;
