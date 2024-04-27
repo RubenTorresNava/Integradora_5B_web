8187ac88605912416f3e48c332f2e46f196fcf0e
Ruben Torres <rubenbebe.145.rt@gmail.com>
2024-04-26 20:45:02 -0600

    Eliminadas rutas y controladores de cadenas de autenticación

--- src/routes/auth.cadena.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import * as authCadenaController from '../controller/auth.cadena.controller.js';
-
-const router = Router();
-
-router.post('/procesarCadena', authCadenaController.procesarCadena);
-router.get('/obtenerCadena', authCadenaController.obtenerCadena);
-
-export default router;
--- src/routes/auth.cadenaAlumn.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import * as authCadenaAlumnController from "../controller/auth.cadenaAlumn.controller.js";
-
-const router = Router();
-
-router.post("/procesarCadenaAlumn", authCadenaAlumnController.procesarCadenaAlumn);
-router.get("/obtenerCadenasAlumn", authCadenaAlumnController.obtenerCadenasAlumn);
-export default router;
